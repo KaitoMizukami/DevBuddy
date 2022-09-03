76168e3f7e321d477de2775513837aed3e8cf87c
KaitoMizukami <kaito.program@gmail.com>
2022-09-04 01:31:40 +0900

    Create custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser, PermissionsMixin
+)
 
-# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=250, unique=True)
+    username = models.CharField(max_length=150)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
