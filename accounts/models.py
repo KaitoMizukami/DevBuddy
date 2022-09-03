3c6a71f755e3520ffa162eb22af3e4896e91b658
KaitoMizukami <kaito.program@gmail.com>
2022-09-04 01:50:56 +0900

    Create UserManager

--- accounts/models.py
+++ accounts/models.py
@@ -4,9 +4,33 @@ from django.contrib.auth.models import (
 )
 
 
+class UserManager(BaseUserManager):
+
+    def create_user(self, username, email, password=None):
+        if not email:
+            raise ValueError('Email is required !')
+        if not username:
+            raise ValueError('Username is required !')
+
+        email = self.normalize_email(email)
+        user = self.model(
+            username=username,
+            email=email
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None):
+        email = self.normalize_email(email)
+        user = self.create_user(username=username, email=email)
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=250, unique=True)
     username = models.CharField(max_length=150)
-    is_active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-
