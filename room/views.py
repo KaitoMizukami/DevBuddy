e2342a10cbf3d524862e15615b1441ee0dc5607b
KaitoMizukami <kaito.program@gmail.com>
2022-09-04 16:44:49 +0900

    Create RoomIndexView

--- room/views.py
+++ room/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+)
 
-# Create your views here.
+from .models import Room
+
+
+class RoomIndexView(ListView):
+    template_name = 'room/room_index.html'
+    model = Room
