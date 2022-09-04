fc4048761b4924f729a1a0e7c58343618c83ac12
KaitoMizukami <kaito.program@gmail.com>
2022-09-04 16:45:39 +0900

    Register 3 models for admin site

--- room/admin.py
+++ room/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Room, Message, Language
+
+
+admin.site.register(Room)
+admin.site.register(Message)
+admin.site.register(Language)
