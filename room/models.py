fdb81bae1b4675ced144edb2fd29dae8cafa3d39
KaitoMizukami <kaito.program@gmail.com>
2022-09-04 16:35:42 +0900

    Create room, message, language model

--- room/models.py
+++ room/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+from accounts.models import User
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Room(models.Model):
+    host = models.ForeignKey(User, on_delete=models.CASCADE)
+    participants = models.ManyToManyField(User, related_name='participants', blank=True)
+    language = models.ForeignKey(Language, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=250)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.body
+        
